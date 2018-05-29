fb461bd3465a0e84b3c424b389d363cb247ae4fb
Kushal Das <mail@kushaldas.in>
2018-05-29 07:40:33 +0530

    Adds some code comments

--- qt_example.py
+++ qt_example.py
@@ -13,14 +13,17 @@ class ExampleApp(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super(ExampleApp, self).__init__(parent)
         self.setupUi(self)
+        # Here we are telling to call git_clone method when
+        # someone clicks on the pushButton.
         self.pushButton.clicked.connect(self.git_clone)
 
+    # Here is the actual method which does git clone
     def git_clone(self):
-        git_url = self.lineEdit.text()
-        tmpdir = tempfile.mkdtemp()
+        git_url = self.lineEdit.text()  # Get the git URL
+        tmpdir = tempfile.mkdtemp()  # Creates a temporary directory
         cmd = "git clone {0} {1}".format(git_url, tmpdir)
-        subprocess.check_output(cmd.split())
-        self.textEdit.setText(tmpdir)
+        subprocess.check_output(cmd.split())  # Execute the command
+        self.textEdit.setText(tmpdir)  # Show the output to the user
 
 
 def main():
--- qt_thread_example.py
+++ qt_thread_example.py
@@ -14,13 +14,14 @@ class CloneThread(QThread):
 
     def __init__(self):
         QThread.__init__(self)
-        self.output = ""
         self.git_url = ""
 
+    # run method gets called when we start the thread
     def run(self):
         tmpdir = tempfile.mkdtemp()
         cmd = "git clone {0} {1}".format(self.git_url, tmpdir)
         subprocess.check_output(cmd.split())
+        # git clone done, now inform the main thread with the output
         self.signal.emit(tmpdir)
 
 
@@ -30,19 +31,22 @@ class ExampleApp(QtWidgets.QMainWindow, Ui_MainWindow):
         super(ExampleApp, self).__init__(parent)
         self.setupUi(self)
         self.pushButton.setText("Git clone with Thread")
+        # Here we are telling to call git_clone method when
+        # someone clicks on the pushButton.
         self.pushButton.clicked.connect(self.git_clone)
-        self.git_thread = CloneThread()
+        self.git_thread = CloneThread()  # This is the thread object
+        # Connect the signal from the thread to the finished method
         self.git_thread.signal.connect(self.finished)
 
     def git_clone(self):
-        self.git_thread.git_url = self.lineEdit.text()
-        self.pushButton.setEnabled(False)
-        self.textEdit.setText("Started git clone operation.")
-        self.git_thread.start()
+        self.git_thread.git_url = self.lineEdit.text()  # Get the git URL
+        self.pushButton.setEnabled(False)  # Disables the pushButton
+        self.textEdit.setText("Started git clone operation.")  # Updates the UI
+        self.git_thread.start()  # Finally starts the thread
 
     def finished(self, result):
-        self.textEdit.setText("Cloned at {0}".format(result))
-        self.pushButton.setEnabled(True)
+        self.textEdit.setText("Cloned at {0}".format(result))  # Show the output to the user
+        self.pushButton.setEnabled(True)  # Enable the pushButton
 
 
 def main():
